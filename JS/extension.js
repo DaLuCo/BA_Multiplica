0ce3cd8317e582c8cbb7cd8ce31d6c6f63079277
“Daniela” <daniela.comes@outlook.com>
2023-05-27 13:16:09 -0300

    Cambios 9

--- JS/extension.js
+++ JS/extension.js
@@ -1,4 +1,14 @@
-let saludo = "Hola";
-console.log(saludo);
+let nombre = "Daniela";
+let apellido = "Comes";
+let edad = 35;
+let profesion = "actriz y bailarina";
+console.log("Hola. Mi nombre es "+nombre+" "+apellido+", tengo "+edad+" "+"años "+"y soy "+profesion);
 
-alert("¡Bienvenidos! 👋")
+alert("¡Bienvenidos/as! 👋😊");
+
+let toggle = document.getElementById('toggle');
+toggle.addEventListener('click', (event)=>{
+  let checked= event.target.checked;
+  document.body.classList.toggle('dark');
+    if (checked==true){}
+  })
